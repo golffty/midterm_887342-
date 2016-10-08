1a078691f133f806fb3c6a805c5874d465362aad
golffty <worachai.srivira@gmail.com>
2016-10-08 15:57:55 +0700

    Finnisd

---
+++ main.rb
@@ -0,0 +1,92 @@
+US_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+
+abbr_states = [ ["AK", "Alaska"], 
+                ["AL", "Alabama"], 
+                ["AR", "Arkansas"], 
+                ["AS", "American Samoa"], 
+                ["AZ", "Arizona"], 
+                ["CA", "California"], 
+                ["CO", "Colorado"], 
+                ["CT", "Connecticut"], 
+                ["DC", "District of Columbia"], 
+                ["DE", "Delaware"], 
+                ["FL", "Florida"], 
+                ["GA", "Georgia"], 
+                ["GU", "Guam"], 
+                ["HI", "Hawaii"], 
+                ["IA", "Iowa"], 
+                ["ID", "Idaho"], 
+                ["IL", "Illinois"], 
+                ["IN", "Indiana"], 
+                ["KS", "Kansas"], 
+                ["KY", "Kentucky"], 
+                ["LA", "Louisiana"], 
+                ["MA", "Massachusetts"], 
+                ["MD", "Maryland"], 
+                ["ME", "Maine"], 
+                ["MI", "Michigan"], 
+                ["MN", "Minnesota"], 
+                ["MO", "Missouri"], 
+                ["MS", "Mississippi"], 
+                ["MT", "Montana"], 
+                ["NC", "North Carolina"], 
+                ["ND", "North Dakota"], 
+                ["NE", "Nebraska"], 
+                ["NH", "New Hampshire"], 
+                ["NJ", "New Jersey"], 
+                ["NM", "New Mexico"], 
+                ["NV", "Nevada"], 
+                ["NY", "New York"], 
+                ["OH", "Ohio"], 
+                ["OK", "Oklahoma"], 
+                ["OR", "Oregon"], 
+                ["PA", "Pennsylvania"], 
+                ["PR", "Puerto Rico"], 
+                ["RI", "Rhode Island"], 
+                ["SC", "South Carolina"], 
+                ["SD", "South Dakota"], 
+                ["TN", "Tennessee"], 
+                ["TX", "Texas"], 
+                ["UT", "Utah"], 
+                ["VA", "Virginia"], 
+                ["VI", "Virgin Islands"], 
+                ["VT", "Vermont"], 
+                ["WA", "Washington"], 
+                ["WI", "Wisconsin"], 
+                ["WV", "West Virginia"], 
+                ["WY", "Wyoming"] ]
+                
+                
+print "---------4----------
+"
+US_states.each{|x| 
+        if x.length-1 > 8
+            puts x
+        end
+}
+print "---------5----------
+"
+US_states.each{|x| 
+        if (x[0]=="M" || x[0]=="N" || x[0]=="S") && (x[x.length-1]=="a" || x[x.length-1]=="e" || x[x.length-1]=="i" || x[x.length-1]=="o" ||  x[x.length-1]=="u")
+            puts x
+        end
+}
+print "---------6----------
+"
+
+print "---------7----------
+"
+h=Hash[*abbr_states.flatten]
+h.each{|key,value| 
+puts key+" "+value 
+    
+}
+
+print "---------8----------
+"
+h=Hash[*abbr_states.flatten]
+h.each{|key,value| 
+
+    puts key+" "+value if (key[0].casecmp(value[0])==0) && (key[1].casecmp(value[1])==0)
+    
+}     
